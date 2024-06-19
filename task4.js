24d1238cdd69066c47654d603558c86296ca8262
kewablay <kojokewajunior123@gmail.com>
2024-06-19 07:42:01 +0000

    feat: added timer

--- task4.js
+++ task4.js
@@ -1,20 +1,30 @@
 function createTimer(duration, elementId) {
-    let remainingTime = duration;
-    let element = document.getElementById(elementId);
-  
-    function updateTimer() {
-      if (remainingTime > 0) {
-        element.textContent = remainingTime;
-        remainingTime--;
-      } else {
-        clearInterval(timerId);
-        console.log('Timer finished');
-      }
+  let remainingTime = duration;
+  let element = document.getElementById(elementId);
+
+  function updateTimer() {
+    if (remainingTime >= 0) {
+      element.innerHTML = `<h1 class="p-10 mb-2 bg-slate-100 rounded-lg w-32 flex justify-center items-center text-4xl font-bold">${remainingTime}</h1>`;
+      // element.textContent = remainingTime;
+      remainingTime--;
+    } else {
+      clearInterval(timerId);
+      let status = document.createElement("p");
+      status.textContent = "Timer finished";
+      status.style.color = "green";
+      element.appendChild(status);
+      console.log("Timer finished");
     }
-  
-    let timerId = setInterval(updateTimer, 1000);
   }
-  
-  
-  createTimer(10, 'timerElement');
-  
+
+  let timerId = setInterval(updateTimer, 1000);
+}
+
+let timerInputForm = document.getElementById("timer-input-form");
+timerInputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let number = document.getElementById("number").value;
+  createTimer(number, "timer-container");
+});
+
+// createTimer(10, "timer-container");
