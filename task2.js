fd61825b6e168da0ce9142b30e8f2d49ed0e5e8b
kewablay <kojokewajunior123@gmail.com>
2024-06-19 08:26:25 +0000

    mod: added comments for task 2

--- task2.js
+++ task2.js
@@ -2,7 +2,7 @@ function handleClick() {
   console.log(this.id);
   console.log(this.textContent);
 
-  //   just to show output on the ui
+  // show output on the ui
   const outputConsole = document.querySelector(".console");
   outputConsole.innerHTML = `
     <p>Id: <span class="font-medium ml-2 text-sm px-2   bg-gray-300">${this.id}</span></p>
@@ -13,17 +13,19 @@ function handleClick() {
 const button = document.querySelector("#button");
 button.addEventListener("click", handleClick);
 
-
-
 const buttonWithArrowFunc = document.querySelector("#buttonWithArrowFunc");
 buttonWithArrowFunc.addEventListener("click", () => {
   console.log(this.id);
   console.log(this.textContent);
 
-  //   just to show output on the ui
+  //  we get  undefined for this.id and this.textContent because arrow functions do not have their own this context. 
+//   Rather, they inherit the this value from the enclosing lexical context. in this case "this" refers to the window object
+
+  // show output on the ui
   const outputConsole = document.querySelector(".console2");
   outputConsole.innerHTML = `
     <p>Id: <span class="font-medium ml-2 text-sm px-2   bg-gray-300">${this.id}</span></p>
     <p>Text Content: <span class="font-medium ml-2 text-sm px-2  bg-gray-300">${this.textContent}</span></p>
+    <p class="text-red-500 mt-4 text-sm">we get  undefined for this.id and this.textContent because arrow functions do not have their own this context.Rather, they inherit the this value from the enclosing lexical context. in this case "this" refers to the window object. </p>
     `;
 });
